fea8d93bec272b8ce484e679635ce86e42c14039
h0p-fr0g <martinbraun1997@gmail.com>
2025-10-18 10:48:29 +0200

    added closing db

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,15 +3,21 @@ public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            ManagePersonalImplementation manager = new ManagePersonalImplementation();
+        ManagePersonalImplementation manager = null;
 
+        try {
+
+            manager = new ManagePersonalImplementation();
             manager.deleteSalesMan(5);
 
-            manager.close();
         }
         catch(ManagePersonalException e){
             System.out.println(e.getMessage());
+        }
+        finally {
+            if (manager != null) {
+                manager.close();
+            }
         }
     }
 }
